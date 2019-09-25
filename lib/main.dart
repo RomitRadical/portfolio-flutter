20561f77eef7c2e17c24b20fdb28ed264687a460
Radical <romitradical@protonmail.com>
2019-09-25 15:50:50 +0530

    porfolio details text

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,93 @@ class Porfolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('Portfolio'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0,
-      ),
-    );
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('Portfolio'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0,
+        ),
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.person, color: Colors.grey),
+                    SizedBox(width: 5),
+                    Text(
+                      'NAME',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Romit Goswami',
+                  style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      'EMAIL',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'romitgoswami@gmail.com',
+                  style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      'PHONE',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '+919916360735',
+                  style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            )));
   }
 }
